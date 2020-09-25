36fd5719fea549eaef1ca82a18ca92a00b900f62
Rasakhatskiy <saharok.m@yandex.ua>
2020-09-25 22:08:34 +0300

    PriorQueue class documented

--- Labs_S3_OOOP_L01/src/PriorQueue/PriorQueue.hpp
+++ Labs_S3_OOOP_L01/src/PriorQueue/PriorQueue.hpp
@@ -1,4 +1,11 @@
-#pragma once
+/*! Queue with priority base class.
+* \file PriorQueue.hpp
+* \author Maksym Rasakhatskyi
+* \version 1.0 25/09/20
+*/
+
+#ifndef PRIOR_QUEUE_HPP
+#define PRIOR_QUEUE_HPP
 
 #include <string>
 
@@ -6,17 +13,41 @@ template <typename T>
 class PriorQueue
 {
 public:
-	//static PriorQueue fromTree();
-	//static PriorQueue fromList();
-	PriorQueue();
+	/*! Constructor.*/
+	virtual PriorQueue();
+
+	/*! Destructor.*/
+	virtual ~PriorQueue();
+
+	/*! Pushes item with priority to queue and puts it on right position.
+	* \param item Value to store.
+	* \param priority Priority of item.
+	*/
 	virtual void push(T item, unsigned int priority);
+
+	/*! Gets element from queue with highest priority, removes it from queue and shifts queue.
+	* \returns Value that stored.
+	*/
 	virtual T pop();
+
+	/*! Gets element from queue with highest priority, DOESN'T remove it from queue.
+	* \returns Value that stored.
+	*/
 	virtual T predict();
+
+	/*! Prints queue to string in format that matches type of container (list, tree, etc).
+	* \returns Queued structure in the string form.
+	*/
 	virtual std::string print();
 };
 
 template<typename T>
 inline PriorQueue<T>::PriorQueue()
+{
+}
+
+template<typename T>
+inline PriorQueue<T>::~PriorQueue()
 {
 }
 
@@ -42,3 +73,8 @@ inline std::string PriorQueue<T>::print()
 {
 	return std::string();
 }
+
+
+#endif // !PRIOR_QUEUE_HPP
+
+
